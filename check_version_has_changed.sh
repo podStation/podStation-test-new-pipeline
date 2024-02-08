a7364b6d83dc5c753f39251cd0a4aa96e95fbba2
Guilherme Dellagustin <dellagustin@gmail.com>
2024-02-09 00:04:45 +0100

    Testing the pipeline 7

--- check_version_has_changed.sh
+++ check_version_has_changed.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 last_version_tag=$(git describe --tags --abbrev=0)
-current_version=v$(cat ./package.json | jq -r ".version")
+current_version_tag=v$(cat ./package.json | jq -r ".version")
 
-if [ $last_version_tag != $current_version ]
+if [ $last_version_tag != $current_version_tag ]
 then
-    echo "Version changed to" $current_version ", previous was" $last_version_tag
-    export CURRENT_VERSION=$current_version
+    echo "Version changed to" $current_version_tag ", previous was" $last_version_tag
+    export CURRENT_VERSION_TAG=$current_version_tag
 	export VERSION_HAS_CHANGED=1
 else
-	echo "Current version is the same as previous version:" $current_version 
+	echo "Current version is the same as previous version:" $current_version_tag
 fi
