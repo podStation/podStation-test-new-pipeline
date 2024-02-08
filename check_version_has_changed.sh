4f9e25883ca6360c65984459585438ab841c4c08
Guilherme Dellagustin <dellagustin@gmail.com>
2024-02-08 23:09:03 +0100

    Testing the pipeline 3

--- check_version_has_changed.sh
+++ check_version_has_changed.sh
@@ -6,11 +6,8 @@ current_version=v$(cat ./package.json | jq -r ".version")
 if [ $last_version_tag != $current_version ]
 then
     echo "Version changed to" $current_version ", previous was" $last_version_tag
-    export NEW_VERSION=$current_version
-    echo "Creating new git tag"
-    git tag $NEW_VERSION
-    echo "Creating zip file for release"
-    ./create_zip_for_upload.sh
+    export CURRENT_VERSION=$current_version
+	export VERSION_HAS_CHANGED=1
 else
 	echo "Current version is the same as previous version:" $current_version 
 fi
